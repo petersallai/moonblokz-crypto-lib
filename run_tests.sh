fd88619b9d099ea99ea230d73ab68ee5ebdaf82a
Peter Sallai <peter.sallai@badaccess.eu>
2025-05-21 16:36:53 +0200

    removed uneccesary double clean

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,4 @@
 cargo llvm-cov clean
-cargo llvm-cov clean
 rm target/lcov.info
 cargo llvm-cov nextest --no-report --no-default-features --features bls-bls12_381-bls --output-path ./target/lcov.info
 cargo llvm-cov nextest --no-report --no-default-features --features schnorr-malachite --output-path ./target/lcov.info
